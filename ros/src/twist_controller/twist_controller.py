35abbb224ed3ba16b84e37a52276054550f34c53
Tariq <tariq.rafique@tariq.com>
2018-05-27 09:38:30 -0700

    dbw_node walkthrough template filled out

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -35,6 +35,33 @@ class Controller(object):
         pass
 
     def control(self, linear_vel, angular_vel, current_vel, dbw_enabled):
-        # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+        
+        current_vel = self.vel_lpf.filt(current_vel)
+
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
+        vel_error = linear_vel - current_vel
+        self.last_vel = current_vel
+
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0.
+
+        if linear_vel == 0 and current_vel < 0.1:
+            throttle = 0
+            brake = 700 #N*m - to hold the car in place if we are stopped at a red light
+        
+        elif throttle < 0.1 and vel_error < 0
+            throttle = 0
+            decel = max(vel_error, self.decel_limit)
+            brake = abs(decel) * self.vehicle_mass * self.wheel_radius
+
+        return throttle, brake, steering
