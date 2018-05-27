870bfb42a2ba56c4320f542f2e0b6d34ed3fb784
Tariq <tariq.rafique@tariq.com>
2018-05-27 15:33:17 -0400

    dbw_node - tweak controller parameters

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,9 +12,9 @@ class Controller(object):
                 wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         
-        kp = 0.3
-        ki = 0.1
-        kd = 0.
+        kp = 0.4
+        ki = 0.0
+        kd = 0.2
         mn = 0. # minimum throttle value
         mx = 0.2 # maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
@@ -37,12 +37,12 @@ class Controller(object):
     def control(self, linear_vel, angular_vel, current_vel, dbw_enabled):
         # Return throttle, brake, steer
 
+        # filtering goes on always. Doesn't matter if dbw is off
+        current_vel = self.vel_lpf.filt(current_vel)
         if not dbw_enabled:
             self.throttle_controller.reset()
             return 0., 0., 0.
         
-        current_vel = self.vel_lpf.filt(current_vel)
-
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
         vel_error = linear_vel - current_vel
@@ -53,15 +53,16 @@ class Controller(object):
         self.last_time = current_time
 
         throttle = self.throttle_controller.step(vel_error, sample_time)
-        brake = 0.
+        brake = 0.0
 
-        if linear_vel == 0 and current_vel < 0.15:
-            throttle = 0
+        if abs(linear_vel) <= 0.05 and current_vel < 0.15:
+            throttle = 0.0
             brake = 700 #N*m - to hold the car in place if we are stopped at a red light
         elif throttle >= 0.1:
             brake = 0.0
         elif throttle < 0.1 and vel_error < 0:
-            throttle = 0
+            throttle = 0.0
+            # decel_limit < 0, so select less negative deceleration
             decel = max(vel_error, self.decel_limit)
             brake = abs(decel) * self.vehicle_mass * self.wheel_radius
         # rospy.logwarn('Throttle: %s', throttle)
