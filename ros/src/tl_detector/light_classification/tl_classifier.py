c098f5e369fdb47cd93a289126eeb8df25a0930b
Tariq <tariq.rafique@tariq.com>
2018-05-28 16:11:50 -0400

    tl_classifier working

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,21 +1,31 @@
 from styx_msgs.msg import TrafficLight
 import tensorflow as tf
 import numpy as np
+import rospy
 
 class TLClassifier(object):
+    
+    min_score = 0.2
+    map_index_TrafficLight = {
+        1 : TrafficLight.GREEN,
+        2 : TrafficLight.RED,
+        3 : TrafficLight.YELLOW,
+        4 : TrafficLight.UNKNOWN
+    }
+
     def __init__(self, model_graph):
         #Initialize tensorflow to use checked in model graph
+        rospy.logwarn("Loading Model: %s", model_graph)
         graph = tf.Graph()
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
         with graph.as_default():
             od_graph_def = tf.GraphDef()
             with tf.gfile.GFile(model_graph, 'rb') as graph_file:
-                od_graph_def.ParseFromString(graph_file.read)
+                od_graph_def.ParseFromString(graph_file.read())
                 tf.import_graph_def(od_graph_def, name='')
         self.image_tensor = graph.get_tensor_by_name('image_tensor:0')
         self.other_tensors = [ 
-            graph.get_tensor_by_name('detection_boxes:0'), 
             graph.get_tensor_by_name('detection_scores:0'),
             graph.get_tensor_by_name('detection_classes:0'),
             graph.get_tensor_by_name('num_detections:0')
@@ -33,14 +43,16 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        (boxes, scores, classes, detections) = self.session.run(self.other_tensors, feed_dict={ self.image_tensor: np.expand_dims(image, axis=0)})
-        boxes = np.squeeze(boxes)
+        (scores, classes, detections) = self.session.run(self.other_tensors, feed_dict={ self.image_tensor: np.expand_dims(image, axis=0)})
         scores = np.squeeze(scores)
         classes = np.squeeze(classes).astype(np.int32)
-        num = num[0]
+        num_detections = detections[0]
         
-        if num > 0:
+        if num_detections > 0:
             # Grab the first light
-            
-        else:
-            return TrafficLight.UNKNOWN
+            score = scores[0]
+            light_state = self.map_index_TrafficLight[classes[0]]
+            if score > self.min_score:
+                return light_state
+        
+        return TrafficLight.UNKNOWN
